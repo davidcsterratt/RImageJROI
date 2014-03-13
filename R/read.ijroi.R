b5592f48018d8f8478a4b004bce0ad1708a78551
David C. Sterratt <david.c.sterratt@ed.ac.uk>
2014-03-13 12:46:41 +0000

    Removed redundant copyright header

--- R/read.ijroi.R
+++ R/read.ijroi.R
@@ -1,19 +1,3 @@
-##    readroi.R - Read ImageJ files in to R
-##    Copyright (C) 2011, 2014 David C. Sterratt <david.c.sterratt@ed.ac.uk>
-
-##    This program is free software: you can redistribute it and/or modify
-##    it under the terms of the GNU General Public License as published by
-##    the Free Software Foundation, either version 3 of the License, or
-##    (at your option) any later version.
-
-##    This program is distributed in the hope that it will be useful,
-##    but WITHOUT ANY WARRANTY; without even the implied warranty of
-##    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-##    GNU General Public License for more details.
-
-##    You should have received a copy of the GNU General Public License
-##    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 ##  ImageJ/NIH Image 64 byte ROI outline header
 ##     2 byte numbers are big-endian signed shorts
 ##     0-3     "Iout"
