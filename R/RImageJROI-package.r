66b3fc78fdc30224daeac32c9b844e5f741cf3cf
David C Sterratt <david.c.sterratt@ed.ac.uk>
2024-08-14 09:12:48 +0100

    Issue #17: Writing ROI files package documentation update

--- R/RImageJROI-package.r
+++ R/RImageJROI-package.r
@@ -1,8 +1,22 @@
-#' Read ImageJ Region of Interest (ROI) files
-#' 
-#' Provides functions to read ImageJ (http://imagej.nih.gov/ij/) Region of Interest (ROI) files, to plot the ROIs and to convert them as spatstat (http://spatstat.org/) spatial patterns.
-#' 
-#' ImageJ ROI objects can be read to R using \code{\link{read.ijroi}} and \code{\link{read.ijzip}} functions. The objects can be plotted using generic \code{\link[=plot.ijroi]{plot}} command and converted to \link{spatstat.geom} spatial patterns by using \code{\link{ij2spatstat}} function.
+#' Read and write 'ImageJ' Region of Interest (ROI) files
+#'
+#' Provides functions to read and write 'ImageJ'
+#' (http://imagej.nih.gov/ij/) Region of Interest (ROI) files, to plot
+#' the ROIs and to convert them as spatstat (http://spatstat.org/)
+#' spatial patterns.
+#'
+#' ImageJ ROI files can be read into R using the
+#' \code{\link{read.ijroi}} and \code{\link{read.ijzip}} functions,
+#' resulting in \code{ijroi} and \code{ijzip} objects.
+#'
+#' The objects can be plotted using generic
+#' \code{\link[=plot.ijroi]{plot}} command and converted to
+#' \link{spatstat.geom} spatial patterns by using
+#' \code{\link{ij2spatstat}} function.
+#'
+#' The \code{ijroi} and \code{ijzip} objects can be written to file
+#' using the \code{\link{write.ijroi}} and \code{\link{write.ijzip}}
+#' functions.
 #'
 #' @name RImageJROI
 #' @docType package
